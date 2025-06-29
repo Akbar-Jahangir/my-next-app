230ef838fa8c5240cced2e016346a9378a40098d
Akbar-Jahangir <aliakbar24589@gmail.com>
2025-06-30 04:06:47 +0500

    updated

---
+++ app/page.js
@@ -0,0 +1,7 @@
+export default function Page() {
+  return <div class="py-4">
+    <h1 class="font-semibold text-2xl pb-2">This is a sample full-stack Next.js application on Azure Static Web Apps!</h1>
+    <p>Check out the <a href="/items" class="text-blue-500">Items</a> page to see CRUD functionality and server-side rendering.</p>
+  </div>
+
+}
